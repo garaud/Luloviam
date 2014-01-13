d4c9b94f9b185a90af6b3e0650c17116fbb4bb0d
Damien Garaud <damien.garaud@gmail.com>
2014-01-13 23:17:37 +0100

    [lexer] First step to handle the 'def' token.

--- src/kalexer.h
+++ src/kalexer.h
@@ -4,6 +4,7 @@
 struct KaToken {
     enum {
         EndOfFile = -1,
+        DefToken = 10,
     };
 };
 
@@ -11,8 +12,12 @@ class KaLexer {
 public:
     static int get(std::istream& data) {
        char last_char;
+       std::string identifier;
        while (!data.eof()) {
           data.get(last_char);
+          identifier += last_char;
+          if (identifier == "def")
+             return 10;
        };
        return -1;
     }
--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -9,3 +9,9 @@ TEST(Lexer, read_eof_token) {
    std::stringstream buffer;
    ASSERT_EQ(KaLexer::get(buffer), KaToken::EndOfFile);
 }
+
+TEST(Lexer, read_def_token) {
+   std::stringstream buffer;
+   buffer << "def foobar(x)";
+   ASSERT_EQ(KaLexer::get(buffer), KaToken::DefToken);
+}
