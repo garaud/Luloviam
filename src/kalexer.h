e9d888348f3b9f22bf6b8f2e458ffda281ee0c81
Damien Garaud <damien.garaud@gmail.com>
2014-02-18 22:21:31 +0100

    Apply BSD C++ indentation style.

--- src/kalexer.h
+++ src/kalexer.h
@@ -21,57 +21,57 @@ struct KaToken {
 };
 
 std::vector<std::string> split_into_word(const std::string& line) {
-   str_container words;
-   std::istringstream stream_line(line);
-   std::copy(sentence_iterator(stream_line),
-                sentence_iterator(),
-                std::back_inserter<str_container>(words));
-   return words;
+    str_container words;
+    std::istringstream stream_line(line);
+    std::copy(sentence_iterator(stream_line),
+              sentence_iterator(),
+              std::back_inserter<str_container>(words));
+    return words;
 }
 
 bool more_than_one_dot(const std::string& str_number) {
-   int dot_count = 0;
-   for (auto num : str_number) {
-      if (num == DECIMAL_SEP)
-         dot_count += 1;
-   }
-   return dot_count > 1;
+    int dot_count = 0;
+    for (auto num : str_number) {
+        if (num == DECIMAL_SEP)
+            dot_count += 1;
+    }
+    return dot_count > 1;
 }
 
 bool first_char_is_not_valid(const std::string str_number) {
-   char first = str_number.front();
-   if (!std::isdigit(first) && first != DECIMAL_SEP
-      && first != MINUS)
-      return true;
-   return false;
+    char first = str_number.front();
+    if (!std::isdigit(first) && first != DECIMAL_SEP
+        && first != MINUS)
+        return true;
+    return false;
 }
 
 bool is_float(const std::string& str_number) {
-   if (more_than_one_dot(str_number))
-       return false;
-   if (first_char_is_not_valid(str_number))
-      return false;
-   for (auto num : str_number.substr(1)) {
-      if (!std::isdigit(num) &&
-          num != DECIMAL_SEP) {
-         return false;
-      }
-   }
-   return true;
+    if (more_than_one_dot(str_number))
+        return false;
+    if (first_char_is_not_valid(str_number))
+        return false;
+    for (auto num : str_number.substr(1)) {
+        if (!std::isdigit(num) &&
+            num != DECIMAL_SEP) {
+            return false;
+        }
+    }
+    return true;
 }
 
 bool is_commented_line(const std::string& line) {
-   if (line.front() == '#')
-      return true;
-   return false;
+    if (line.front() == '#')
+        return true;
+    return false;
 }
 
 bool is_alnum(const std::string& word) {
-   for (auto letter : word) {
-      if (!std::isalnum(letter))
-         return false;
-   }
-   return true;
+    for (auto letter : word) {
+        if (!std::isalnum(letter))
+            return false;
+    }
+    return true;
 }
 
 
@@ -80,27 +80,27 @@ bool is_alnum(const std::string& word) {
 class KaLexer {
 public:
     static int nextToken(std::istream& data) {
-       while (!data.eof()) {
-          std::string sentence;
-          std::getline(data, sentence);
-          if (sentence.empty())
-             continue;
-          str_container words = split_into_word(sentence);
-          if (is_commented_line(words.front()))
+        while (!data.eof()) {
+            std::string sentence;
+            std::getline(data, sentence);
+            if (sentence.empty())
                 continue;
-          for (auto identifier : words) {
-             if (is_float(identifier))
-                return KaToken::NumberToken;
-             if (is_alnum(identifier)) {
-                if (identifier == "def")
-                   return KaToken::DefToken;
-                else if (identifier == "extern")
-                   return KaToken::ExternToken;
-                else
-                   return KaToken::IdentifierToken;
-             }
-          }
-       };
-       return KaToken::EndOfFile;
+            str_container words = split_into_word(sentence);
+            if (is_commented_line(words.front()))
+                continue;
+            for (auto identifier : words) {
+                if (is_float(identifier))
+                    return KaToken::NumberToken;
+                if (is_alnum(identifier)) {
+                    if (identifier == "def")
+                        return KaToken::DefToken;
+                    else if (identifier == "extern")
+                        return KaToken::ExternToken;
+                    else
+                        return KaToken::IdentifierToken;
+                }
+            }
+        };
+        return KaToken::EndOfFile;
     }
 };
--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -10,42 +10,42 @@ const int EQUAL_CHAR('='); // ASCII value 61
 // Testing fixture for the lexer.
 class Lexer: public ::testing::Test {
 public:
-   std::stringstream buffer;
+    std::stringstream buffer;
 };
 
 TEST_F(Lexer, read_eof_token) {
-   ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::EndOfFile);
+    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::EndOfFile);
 }
 
 TEST_F(Lexer, read_def_token) {
-   buffer << "def foobar(x)";
-   ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::DefToken);
+    buffer << "def foobar(x)";
+    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::DefToken);
 }
 
 TEST_F(Lexer, read_extern_token) {
-   buffer << "extern babar();";
-   ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::ExternToken);
+    buffer << "extern babar();";
+    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::ExternToken);
 }
 
 TEST_F(Lexer, skip_trailing_whitespaces) {
-   buffer << "     def jazzy(player, year)";
-   ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::DefToken);
+    buffer << "     def jazzy(player, year)";
+    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::DefToken);
 }
 
 TEST_F(Lexer, recognizes_identifier_token) {
-   buffer << "variable = 12.5";
-   ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::IdentifierToken);
+    buffer << "variable = 12.5";
+    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::IdentifierToken);
 }
 
 TEST_F(Lexer, recognizes_float_number) {
-   buffer << "12.7";
-   ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::NumberToken);
+    buffer << "12.7";
+    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::NumberToken);
 }
 
 TEST_F(Lexer, not_take_into_account_commented_line) {
-   buffer << "  #   def stuff" << std::endl
-          << "extern get_name(a, x)" << std::endl;
-   ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::ExternToken);
+    buffer << "  #   def stuff" << std::endl
+           << "extern get_name(a, x)" << std::endl;
+    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::ExternToken);
 }
 
 TEST_F(Lexer, not_identified_char_must_return_ascii_value) {
@@ -55,29 +55,29 @@ TEST_F(Lexer, not_identified_char_must_return_ascii_value) {
 }
 
 TEST(FuncUtils, split_line_into_word) {
-   std::string line("just three  words");
-   std::vector<std::string> words = split_into_word(line);
-   ASSERT_EQ(3, words.size());
+    std::string line("just three  words");
+    std::vector<std::string> words = split_into_word(line);
+    ASSERT_EQ(3, words.size());
 }
 
 TEST(FuncUtilsIsAlNum, recognize_alnum_word) {
-   std::string word = "nameJohn12";
-   ASSERT_TRUE(is_alnum(word));
+    std::string word = "nameJohn12";
+    ASSERT_TRUE(is_alnum(word));
 }
 
 TEST(FuncUtilIsFloat, well_recognize_float_number_in_a_string) {
-   std::string str_number("14.702");
-   ASSERT_TRUE(is_float(str_number));
+    std::string str_number("14.702");
+    ASSERT_TRUE(is_float(str_number));
 }
 
 TEST(FuncUtilIsFloat, handle_signed_float) {
-   std::string str_number("-12.05");
-   ASSERT_TRUE(is_float(str_number));
+    std::string str_number("-12.05");
+    ASSERT_TRUE(is_float(str_number));
 }
 
 TEST(FuncUtilIsFloat, dont_recognize_malformed_str_float) {
-   std::string str_number("45.-05");
-   ASSERT_TRUE(!is_float(str_number));
-   str_number = "123.45.78";
-   ASSERT_TRUE(!is_float(str_number));
+    std::string str_number("45.-05");
+    ASSERT_TRUE(!is_float(str_number));
+    str_number = "123.45.78";
+    ASSERT_TRUE(!is_float(str_number));
 }
