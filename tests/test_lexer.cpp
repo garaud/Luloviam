85d21c3f51cdf3d745cd9e6629322173938eaaef
Damien Garaud <damien.garaud@gmail.com>
2014-02-26 23:27:29 +0100

    [test] Swap the arguments of ASSERT_EQ for several test cases.
    The first one is the expected value whereas the second one is the
    tested/actual value.

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -14,50 +14,50 @@ public:
 };
 
 TEST_F(Lexer, read_eof_token) {
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::EndOfFile);
+    ASSERT_EQ(KaToken::EndOfFile, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, read_def_token) {
     buffer << "def foobar(x)";
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::DefToken);
+    ASSERT_EQ(KaToken::DefToken, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, read_extern_token) {
     buffer << "extern babar();";
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::ExternToken);
+    ASSERT_EQ(KaToken::ExternToken, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, skip_trailing_whitespaces) {
     buffer << "     def jazzy(player, year)";
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::DefToken);
+    ASSERT_EQ(KaToken::DefToken, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, recognizes_identifier_token) {
     buffer << "variable = 12.5";
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::IdentifierToken);
+    ASSERT_EQ(KaToken::IdentifierToken, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, recognizes_float_number) {
     buffer << "12.7";
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::NumberToken);
+    ASSERT_EQ(KaToken::NumberToken, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, not_take_into_account_commented_line) {
     buffer << "  #   def stuff" << std::endl
            << "extern get_name(a, x)" << std::endl;
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::ExternToken);
+    ASSERT_EQ(KaToken::ExternToken, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, not_identified_char_must_return_unknown_token) {
     buffer << "= 13.37";
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::Unknown);
+    ASSERT_EQ(KaToken::Unknown, KaLexer::nextToken(buffer));
 }
 
 TEST_F(Lexer, DISABLED_consecutive_call_return_the_well_serie_of_tokens) {
     buffer << "babar = 23.0";
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::IdentifierToken);
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::Unknown);
-    ASSERT_EQ(KaLexer::nextToken(buffer), KaToken::NumberToken);
+    ASSERT_EQ(KaToken::IdentifierToken, KaLexer::nextToken(buffer));
+    ASSERT_EQ(KaToken::Unknown, KaLexer::nextToken(buffer));
+    ASSERT_EQ(KaToken::NumberToken, KaLexer::nextToken(buffer));
 }
 
 TEST(FuncUtils, split_line_into_word) {
