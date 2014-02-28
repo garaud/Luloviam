8b48b1c2be239c6e5afbf1bfd65561fc6eaea1a9
Damien Garaud <damien.garaud@gmail.com>
2014-02-28 18:10:43 +0100

    [lexer] Move the string utils functions into a dedicated file.

--- src/kalexer.h
+++ src/kalexer.h
@@ -1,14 +1,7 @@
 
 #include <string>
-#include <vector>
-#include <algorithm>
-#include <iterator>
 
-typedef std::istream_iterator<std::string> sentence_iterator;
-typedef std::vector<std::string> str_container;
-
-const int DECIMAL_SEP('.');
-const int MINUS('-');
+#include "str_utils.h"
 
 enum class KaToken {
         EndOfFile = -1,
@@ -18,61 +11,6 @@ enum class KaToken {
         NumberToken = -20,
         Unknown = -99,
 };
-
-std::vector<std::string> split_into_word(const std::string& line) {
-    str_container words;
-    std::istringstream stream_line(line);
-    std::copy(sentence_iterator(stream_line),
-              sentence_iterator(),
-              std::back_inserter<str_container>(words));
-    return words;
-}
-
-bool more_than_one_dot(const std::string& str_number) {
-    int dot_count = 0;
-    for (auto num : str_number) {
-        if (num == DECIMAL_SEP)
-            dot_count += 1;
-    }
-    return dot_count > 1;
-}
-
-bool first_char_is_not_valid(const std::string str_number) {
-    char first = str_number.front();
-    if (!std::isdigit(first) && first != DECIMAL_SEP
-        && first != MINUS)
-        return true;
-    return false;
-}
-
-bool is_float(const std::string& str_number) {
-    if (more_than_one_dot(str_number))
-        return false;
-    if (first_char_is_not_valid(str_number))
-        return false;
-    for (auto num : str_number.substr(1)) {
-        if (!std::isdigit(num) &&
-            num != DECIMAL_SEP) {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool is_commented_line(const std::string& line) {
-    if (line.front() == '#')
-        return true;
-    return false;
-}
-
-bool is_alnum(const std::string& word) {
-    for (auto letter : word) {
-        if (!std::isalnum(letter))
-            return false;
-    }
-    return true;
-}
-
 
 
 //! Lexer class.
